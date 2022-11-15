e5aefdf947163bb6c419d1932dbd0ff46c1b5257
YijieChen720 <cccyyj720@gmail.com>
2022-11-14 21:23:16 -0500

    feat: add makefile to compile cuda code

---
+++ src/cudaGeneralHoughTransform.cu
@@ -0,0 +1,25 @@
+#include "cudaGeneralHoughTransform.h"
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <driver_functions.h>
+
+CudaGeneralHoughTransform::CudaGeneralHoughTransform() {
+}
+
+CudaGeneralHoughTransform::~CudaGeneralHoughTransform() {
+}
+
+void CudaGeneralHoughTransform::processTemplate() {
+}
+
+void CudaGeneralHoughTransform::accumulateSource() {
+}
+
+bool CudaGeneralHoughTransform::loadTemplate(std::string filename) {
+    return false;
+}
+
+bool CudaGeneralHoughTransform::loadSource(std::string filename) {
+    return false;
+}
